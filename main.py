69215196b0c3465ce01698e3be2533c443695011
Roger0-0 <75006171+Roger0-0@users.noreply.github.com>
2020-12-23 05:56:53 -0500

    Add files via upload
    The calculator actually exists now.

--- main.py
+++ main.py
@@ -1,4 +1,38 @@
+import kivy
+from kivy.app import App
+#from kivy.core.window import Window
+from kivy.uix.widget import Widget
+from kivy.modules import keybinding
 import numpy as np
-def main():
+import matplotlib.pyplot as plt
 
-main()
+
+class CalcLayout(Widget):
+    def graph(self,expression):
+        try:
+            y = eval(expression)
+            plt.plot(y)
+            plt.ylabel('y') 
+            plt.show()
+
+        except Exception:
+            self.display.text="Error\nMake sure all parentheses are closed"
+            
+
+    def calculate(self,expression):
+        if expression:
+            try:
+                self.display.text=str(eval(expression))
+            except Exception:
+                self.display.text="Error"
+        
+
+class CalcApp(App):
+
+    def build(self):
+        
+        return CalcLayout()
+
+
+if __name__=='__main__':
+    CalcApp().run()
